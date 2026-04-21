bebecf203d9cbe2fe661c45c4263cbcdd5605a6e
Amel90 <ameldany18@gmail.com>
2026-04-21 00:10:54 -0400

    Changed the HTLM for the site

--- Amelapp/models.py
+++ Amelapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.title
