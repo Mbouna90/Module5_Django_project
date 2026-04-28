c633fb89b5f8164edc8d9d9930a0623dd0ad047c
Amel90 <ameldany18@gmail.com>
2026-04-27 21:46:31 -0400

    completed Django template and styling

--- Amelapp/views.py
+++ Amelapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def home(request):
     return render(request, 'Amelapp/home.html')
+
+def post_list(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()
+    ).order_by('published_date')
+
+    return render(request, 'Amelapp/post_list.html', {'posts': posts})
